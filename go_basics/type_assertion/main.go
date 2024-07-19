7bacfee59ee528d903ae0a58924609f4bb512919
zombiehunter <viktormallory25@gmail.com>
2024-07-19 19:40:23 +0300

    type_assertion

---
+++ go_basics/type_assertion/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Article struct {
+	Title  string
+	Author string
+}
+
+func (a Article) String() string {
+	return fmt.Sprintf("The %s article was written by %s", a.Title, a.Author)
+}
+
+// func typeCast(s fmt.Stringer) {
+// 	article := s.(Article)
+// 	fmt.Printf("%+v\n", article.Title)
+// 	// fmt.Printf("%+v\n", s.Title)
+// }
+
+func typeCast(s fmt.Stringer) {
+	article, ok := s.(Article) // article == nil, if not ok
+	if ok {
+		fmt.Printf("%+v\n", article.Title)
+	} else {
+		fmt.Println("Invalid type assertion")
+	}
+}
+
+func main() {
+	a := Article{
+		Title:  "Go for beginners",
+		Author: "James Bond",
+	}
+
+	typeCast(a)
+}
